cead39783b9a20580700225853abd3a6acba31c2
Marc Bluemner <marc.bluemner@gmail.com>
2015-09-01 06:51:40 +0200

    routes

--- web/router.ex
+++ web/router.ex
@@ -1,16 +1,42 @@
 defmodule Blog.Router do
   use Blog.Web, :router
 
+
   pipeline :browser do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Blog.Plug.CSRF
+  end
+
+  pipline :auth do
+    plug Blog.Plug.Authentication
   end
 
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  # public routes via the api
+  scope "/api/v1", alias: Blog do
+    pipe_through :api
+  end
+
+  # Private routes via the api
+  scope "/api/v1", alias: Blog do
+    pipe_through[:api, :auth]
+  end
+
+  # Public routes via the browser
+  scope alias: Blog do
+    pipe_through :browser
+  end
+
+  # Private routes via the browser
+  scope alias: Blog do
+    pipe_through[:browser, :auth]
   end
 
   scope "/", Blog do
